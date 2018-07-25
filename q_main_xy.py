29fa6bcf64da2ca4d609607c46ddcd1bd643964a
PaulMcHard <paulmchard@gmail.com>
2018-07-25 12:10:37 +0100

    began working on xy q table implementation

--- q_main_xy.py
+++ q_main_xy.py
@@ -15,10 +15,12 @@ def chunk_list(list, n):
 class dataset():
     def __init__(self, data):
         self.xcom = data[:,0]
-        self.xfbk = data[:,1]
-        self.init_mean = np.mean(np.absolute(self.xcom-self.xfbk))
-        self.num_steps = len(self.xcom)
-        self.errors = np.zeros(self.num_steps)
+        self.ycom = data[:,1]
+        self.xfbk = data[:,2]
+        self.yfbk = data[:,3]
+        self.init_mean = (np.mean(np.absolute(self.xcom-self.xfbk)),np.mean(np.absolute(self.ycom-self.yfbk)))
+        self.num_steps = len(self.xcom) #num_steps is constant for x and y
+        self.errors = np.zeros((2,self.num_steps))
         self.mean_errors = []
 
     def pad_size(self, max_steps):
@@ -26,7 +28,9 @@ class dataset():
         filler = np.zeros(difference)
         self.xcom = np.concatenate((self.xcom, filler))
         self.xfbk = np.concatenate((self.xfbk, filler))
-        self.errors = np.concatenate((self.errors, filler))
+        self.ycom = np.concatenate((self.ycom, filler))
+        self.yfbk = np.concatenate((self.yfbk, filler))
+        np.lib.pad(self.errors, (0,len(filler)), 'constant', constant_values=(0))
         self.num_steps = len(self.xcom)
 
 actions = [1, .9, .8, .7, .6, .5, .4, .3, .2, .1, 0, -.1, -.2, -.3, -.4, -.5, -.6, -.7, -.8, -.9, -1]
@@ -42,7 +46,7 @@ for file in os.listdir(dirname):
 file_data = []
 for file in files:
     data_parser.parse_data(dirname+file)
-    temp = data_parser.get_x().values
+    temp = data_parser.get_all_com_fbk().values
     file_data.append(temp)
 
 set_length = 500
@@ -58,7 +62,8 @@ for file in file_data:
 random.shuffle(datasets)
 
 state_size = set_length
-qtable = np.zeros((state_size, action_size))
+qtable = np.zeros((2,state_size, action_size))
+
 
 #Function below gives our reward system.
 def check_error(e_n, e_nplus):
@@ -88,10 +93,11 @@ decay_rate = 0.01   #Exponential decayrate for exploration prob
 #List of rewards
 rewards = []
 
-overall_init_mean = 0
-sum = 0
+overall_init_mean = np.zeros(2)
+sum = np.zeros(2)
 for set in datasets:
-    sum += set.init_mean
+    for i in range(0,1):
+        sum += set.init_mean
 overall_init_mean = sum/len(datasets)
 
 mean_errors = np.zeros((len(datasets), total_epochs))
@@ -100,7 +106,7 @@ for epoch in range(total_epochs):
     #Reset the environment
     step = 0
     done = False
-    total_rewards = 0
+    total_rewards = np.zeros(2)
 
     for set in range(len(datasets)):
 
@@ -111,24 +117,31 @@ for epoch in range(total_epochs):
 
             ##If this number > greater than epsilon --> exploitation (taking the biggest Q value for this state)
             if exp_exp_tradeoff > epsilon:
-                action = np.argmax(qtable[step, :])
+                x_action = np.argmax(qtable[0,step, :])
+                y_action = np.argmax(qtable[1,step, :])
 
             #Else doing a random choice --> exploration
             else:
-                action = np.random.randint(action_size)
+                x_action = np.random.randint(action_size)
+                y_action = np.random.randint(action_size)
 
             #Take the action a and observe the outcome state s' and reward r
-            datasets[set].xfbk[step] += actions[action]*np.sign(datasets[set].xcom[step]-datasets[set].xfbk[step]) #add 1, 0 or -1 to demonstrate accel, const or decel
-            datasets[set].errors[step] = np.absolute(datasets[set].xcom[step]-datasets[set].xfbk[step])
-            datasets[set].errors[step+1] = np.absolute(datasets[set].xcom[step+1]-datasets[set].xfbk[step+1])
-            reward = check_error(datasets[set].errors[step], datasets[set].errors[step+1])
+            datasets[set].xfbk[step] += actions[x_action]*np.sign(datasets[set].xcom[step]-datasets[set].xfbk[step]) #add 1, 0 or -1 to demonstrate accel, const or decel
+            datasets[set].yfbk[step] += actions[y_action]*np.sign(datasets[set].ycom[step]-datasets[set].yfbk[step])
+            datasets[set].errors[0,step] = np.absolute(datasets[set].xcom[step]-datasets[set].xfbk[step])
+            datasets[set].errors[1,step] = np.absolute(datasets[set].ycom[step]-datasets[set].yfbk[step])
+            datasets[set].errors[0,step+1] = np.absolute(datasets[set].xcom[step+1]-datasets[set].xfbk[step+1])
+            datasets[set].errors[1,step+1] = np.absolute(datasets[set].ycom[step+1]-datasets[set].yfbk[step+1])
+            x_reward = check_error(datasets[set].errors[0,step], datasets[set].errors[0,step+1])
+            y_reward = check_error(datasets[set].errors[1,step], datasets[set].errors[1,step+1])
 
 
             # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
             # qtable[new_state,:] : all the actions we can take from new state
-            qtable[step,action] = qtable[step, action] + learning_rate*(reward + gamma * np.max(qtable[step+1, :]) - qtable[step,action])
+            qtable[0, step, x_action] = qtable[0, step, x_action] + learning_rate*(x_reward + gamma * np.max(qtable[0,step+1, :]) - qtable[0,step,x_action])
+            qtable[1, step, y_action] = qtable[1, step, y_action] + learning_rate*(y_reward + gamma * np.max(qtable[0,step+1, :]) - qtable[0,step,y_action])
 
-            total_rewards += reward
+            total_rewards += (x_reward, y_reward)
 
         n_mean_step = np.mean(np.absolute(datasets[set].errors))
         datasets[set].mean_errors.append(n_mean_step)
